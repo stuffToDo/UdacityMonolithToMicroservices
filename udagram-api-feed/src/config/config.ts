cd838cb8fd521a550016e1acd53a46279d2d6a7e
jason <justaguynamedjason@gmail.com>
2021-01-03 17:17:21 -0600

    Adding split api projects

---
+++ udagram-api-feed/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': process.env.DB_USERNAME,
+  'password': process.env.DB_PASSWORD,
+  'database': process.env.DB_NAME,
+  'host': process.env.DB_HOST,
+  'dialect': process.env.DB_DIALECT,
+  'aws_region': process.env.AWS_REGION,
+  'aws_profile': process.env.AWS_PROFILE,
+  'aws_media_bucket': process.env.AWS_BUCKET,
+  'url': process.env.URL,
+  'jwt': {
+    'secret': process.env.JWT_TOKEN,
+  },
+};
